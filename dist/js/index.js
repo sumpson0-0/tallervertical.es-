3602b40f2d6835b085bab923a1ff30027e2354d4
sumpson0-0 <sumpson00@gmail.com>
2020-07-07 21:43:57 +0900

    Javascript scrollEvent

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,1 +1,56 @@
 "use strict";
+
+var isWorking = document.querySelector('.is-working');
+var currentValue = isWorking.lastElementChild.firstElementChild;
+var nowScroll = window.scrollY;
+
+var scrollEvent = function scrollEvent(e) {
+  nowScroll = e.path[1].scrollY;
+  init();
+};
+
+var init = function init() {
+  if (nowScroll >= 0 && 100 > nowScroll) {
+    currentValue.innerText = 1;
+  }
+
+  if (nowScroll >= 100 && 200 > nowScroll) {
+    currentValue.innerText = 2;
+  }
+
+  if (nowScroll >= 200 && 300 > nowScroll) {
+    currentValue.innerText = 3;
+  }
+
+  if (nowScroll >= 300 && 400 > nowScroll) {
+    currentValue.innerText = 4;
+  }
+
+  if (nowScroll >= 400 && 500 > nowScroll) {
+    currentValue.innerText = 5;
+  }
+
+  if (nowScroll >= 500 && 600 > nowScroll) {
+    currentValue.innerText = 6;
+  }
+
+  if (nowScroll >= 600 && 700 > nowScroll) {
+    currentValue.innerText = 7;
+  }
+
+  if (nowScroll >= 700 && 800 > nowScroll) {
+    currentValue.innerText = 8;
+  }
+
+  if (nowScroll >= 800 && 900 > nowScroll) {
+    currentValue.innerText = 9;
+  }
+
+  if (nowScroll >= 900 && 1000 >= nowScroll) {
+    currentValue.innerText = 10;
+  }
+
+  window.addEventListener('scroll', scrollEvent);
+};
+
+init();
