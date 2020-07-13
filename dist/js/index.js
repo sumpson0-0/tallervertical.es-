de2b7c74641161b9e560e7b80960122630a2051a
sumpson0-0 <sumpson00@gmail.com>
2020-07-13 20:44:57 +0900

    scroll event 제거 & wheel event 추가
    is-working 클래스명 부여 및 제거

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,82 +3,40 @@
 var isWorking = document.querySelector('.is-working');
 var currentCounter = document.querySelector('.counter-current');
 var contentWrapper = document.querySelector('.content-wrapper');
-var nowScroll = window.scrollY;
 var currentPage;
 
-var giveClassname = function giveClassname() {
-  var contentWrapperChildren = Array.from(contentWrapper.children);
-  contentWrapperChildren.forEach(function (element) {
-    var elementClassName = element.classList; // classList[1]의 element를 가져오게 만들면 될 듯..
+var addClassname = function addClassname() {
+  var contentWrapperItems = Array.from(contentWrapper.children);
+  contentWrapperItems.forEach(function (element) {
+    if (Number(element.classList[1]) === currentPage) {
+      element.classList.add('is-working'); // previousElementSibling이 있는지 확인 후 있으면 제거하도록 지시
 
-    if (Number.parseInt(elementClassName[1]) === currentPage) {// console.log(element);
+      if (element.previousElementSibling) {
+        element.previousElementSibling.classList.remove('is-working');
+      } // nextElementSibling 있는지 확인 후 있으면 제거하도록 지시
+
+
+      if (element.nextElementSibling) {
+        element.nextElementSibling.classList.remove('is-working');
+      }
     }
   });
 };
 
 var onWheelEvent = function onWheelEvent(e) {
-  console.log(e.isTrusted, nowScroll);
-};
+  if (1 < Number(currentCounter.innerText) && Number(currentCounter.innerText) <= 10 && e.wheelDelta > 0) {
+    currentPage = Number(currentCounter.innerText) - 1;
+    currentCounter.innerText = currentPage;
+  } else if (1 <= Number(currentCounter.innerText) && Number(currentCounter.innerText) < 10 && e.wheelDelta < 0) {
+    currentPage = Number(currentCounter.innerText) + 1;
+    currentCounter.innerText = currentPage;
+  }
 
-var scrollEvent = function scrollEvent(e) {
-  nowScroll = e.path[1].scrollY;
-  init(); // 스크롤 하면 그 때 마다 init 재실행되서 init에 있는 함수 다시 실행됨. 주의.
+  addClassname();
 };
 
 var init = function init() {
-  if (nowScroll >= 0 && 100 > nowScroll) {
-    currentCounter.innerText = 1;
-    currentPage = 1; // 문제 스크롤이 진행되는 내내 page가 업데이트 됨 (11)1 이런식..?
-  }
-
-  if (nowScroll >= 100 && 200 > nowScroll) {
-    currentCounter.innerText = 2;
-    currentPage = 2;
-  }
-
-  if (nowScroll >= 200 && 300 > nowScroll) {
-    currentCounter.innerText = 3;
-    currentPage = 3;
-  }
-
-  if (nowScroll >= 300 && 400 > nowScroll) {
-    currentCounter.innerText = 4;
-    currentPage = 4;
-  }
-
-  if (nowScroll >= 400 && 500 > nowScroll) {
-    currentCounter.innerText = 5;
-    currentPage = 5;
-  }
-
-  if (nowScroll >= 500 && 600 > nowScroll) {
-    currentCounter.innerText = 6;
-    currentPage = 6;
-  }
-
-  if (nowScroll >= 600 && 700 > nowScroll) {
-    currentCounter.innerText = 7;
-    currentPage = 7;
-  }
-
-  if (nowScroll >= 700 && 800 > nowScroll) {
-    currentCounter.innerText = 8;
-    currentPage = 8;
-  }
-
-  if (nowScroll >= 800 && 900 > nowScroll) {
-    currentCounter.innerText = 9;
-    currentPage = 9;
-  }
-
-  if (nowScroll >= 900 && 998 >= nowScroll) {
-    currentCounter.innerText = 10;
-    currentPage = 10;
-  }
-
-  window.addEventListener('scroll', scrollEvent);
   window.addEventListener('wheel', onWheelEvent);
-  giveClassname();
 };
 
 init();
--- src/js/index.js
+++ src/js/index.js
@@ -2,76 +2,38 @@ const isWorking = document.querySelector('.is-working');
 const currentCounter = document.querySelector('.counter-current');
 const contentWrapper = document.querySelector('.content-wrapper');
 
-let nowScroll = window.scrollY;
 let currentPage;
 
-const giveClassname = () => {
-	const contentWrapperChildren = Array.from(contentWrapper.children);
-	contentWrapperChildren.forEach(element => {
-		const elementClassName = element.classList;
-		// classList[1]의 element를 가져오게 만들면 될 듯..
-
-		if (Number.parseInt(elementClassName[1]) === currentPage) {
-			// console.log(element);
+const addClassname = () => {
+	const contentWrapperItems = Array.from(contentWrapper.children);
+	contentWrapperItems.forEach(element => {
+		if (Number(element.classList[1]) === currentPage) {
+			element.classList.add('is-working');
+			// previousElementSibling이 있는지 확인 후 있으면 제거하도록 지시
+			if (element.previousElementSibling) {
+				element.previousElementSibling.classList.remove('is-working');
+			}
+			// nextElementSibling 있는지 확인 후 있으면 제거하도록 지시
+			if (element.nextElementSibling) {
+				element.nextElementSibling.classList.remove('is-working');
+			}
 		}
 	});
 };
 
 const onWheelEvent = e => {
-	console.log(e.isTrusted, nowScroll);
-};
-
-const scrollEvent = e => {
-	nowScroll = e.path[1].scrollY;
-	init();
-	// 스크롤 하면 그 때 마다 init 재실행되서 init에 있는 함수 다시 실행됨. 주의.
+	if (1 < Number(currentCounter.innerText) && Number(currentCounter.innerText) <= 10 && e.wheelDelta > 0) {
+		currentPage = Number(currentCounter.innerText) - 1;
+		currentCounter.innerText = currentPage;
+	} else if (1 <= Number(currentCounter.innerText) && Number(currentCounter.innerText) < 10 && e.wheelDelta < 0) {
+		currentPage = Number(currentCounter.innerText) + 1;
+		currentCounter.innerText = currentPage;
+	}
+	addClassname();
 };
 
 const init = () => {
-	if (nowScroll >= 0 && 100 > nowScroll) {
-		currentCounter.innerText = 1;
-		currentPage = 1;
-		// 문제 스크롤이 진행되는 내내 page가 업데이트 됨 (11)1 이런식..?
-	}
-	if (nowScroll >= 100 && 200 > nowScroll) {
-		currentCounter.innerText = 2;
-		currentPage = 2;
-	}
-	if (nowScroll >= 200 && 300 > nowScroll) {
-		currentCounter.innerText = 3;
-		currentPage = 3;
-	}
-	if (nowScroll >= 300 && 400 > nowScroll) {
-		currentCounter.innerText = 4;
-		currentPage = 4;
-	}
-	if (nowScroll >= 400 && 500 > nowScroll) {
-		currentCounter.innerText = 5;
-		currentPage = 5;
-	}
-	if (nowScroll >= 500 && 600 > nowScroll) {
-		currentCounter.innerText = 6;
-		currentPage = 6;
-	}
-	if (nowScroll >= 600 && 700 > nowScroll) {
-		currentCounter.innerText = 7;
-		currentPage = 7;
-	}
-	if (nowScroll >= 700 && 800 > nowScroll) {
-		currentCounter.innerText = 8;
-		currentPage = 8;
-	}
-	if (nowScroll >= 800 && 900 > nowScroll) {
-		currentCounter.innerText = 9;
-		currentPage = 9;
-	}
-	if (nowScroll >= 900 && 998 >= nowScroll) {
-		currentCounter.innerText = 10;
-		currentPage = 10;
-	}
-	window.addEventListener('scroll', scrollEvent);
 	window.addEventListener('wheel', onWheelEvent);
-	giveClassname();
 };
 
 init();
